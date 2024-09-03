54d2edf5488d67e37719b54f9cb7377bb173b55d
Arthur Lima <160620274+Arthur-Lima07@users.noreply.github.com>
2024-09-03 14:58:26 -0300

    Add files via upload

---
+++ Animal/src/br/edu/robo/Robo.java
@@ -0,0 +1,5 @@
+package br.edu.robo;
+
+public class Robo {
+
+}
---
+++ Animal/src/br/edu/robo/RoboCachorro.java
@@ -0,0 +1,18 @@
+package br.edu.robo;
+
+import br.edu.interfaces.*;
+
+public class RoboCachorro implements Pet {
+
+
+	public void SerAmigavel() {
+		
+	}
+
+	@Override
+	public void Brincar() {
+		
+	}
+	
+
+}
